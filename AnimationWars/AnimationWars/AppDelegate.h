6e82e9ccd5c59cb375977eccd37205497483d929
钱盼盼 <qpanpan@qiujieda.com>
2020-03-05 17:09:58 +0800

    add project

---
+++ AnimationWars/AnimationWars/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  AnimationWars
+//
+//  Created by qianpanpan on 2020/3/5.
+//  Copyright © 2020 MengLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+
+@end
+
---
+++ AnimationWars/AnimationWars/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  AnimationWars
+//
+//  Created by qianpanpan on 2020/3/5.
+//  Copyright © 2020 MengLA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
